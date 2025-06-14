26532ddec651bb4725ffbc1738dd068a3bd00ce6
SRINU NAYAK <srinunayakk7@gmail.com>
2025-06-13 21:11:58 -0400

    added exception file

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 from src.mlproject.logger import logging
+from src.mlproject.exception import CustomException
+import sys
+
 
 
 if __name__ == "__main__":
     logging.info('project has started')
+
+    try:
+        a = 1/0
+    except Exception as e:
+        logging.error('custom exception occured')
+        raise CustomException(e, sys)
