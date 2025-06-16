7555f9b0c0661ad5cf83302c34b715e32cf5be8f
SRINU NAYAK <srinunayakk7@gmail.com>
2025-06-15 20:00:16 -0400

    data ingestion completed from sql database

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
+from sklearn.model_selection import train_test_split
+
 from src.mlproject.logger import logging
 from src.mlproject.exception import CustomException
 import sys
-
-
+from  src.mlproject.components.data_ingestion import  DataIngestion
+from  src.mlproject.components.data_ingestion import  DataIngestionConfig
 
 if __name__ == "__main__":
     logging.info('project has started')
 
     try:
-        a = 1/0
+        data_ingestion = DataIngestion()
+        data_ingestion.initiate_data_ingestion()
+
+
     except Exception as e:
         logging.error('custom exception occured')
         raise CustomException(e, sys)
